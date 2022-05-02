815f73d539e488dc2f535e90e8f6ca216d934098
User <User@WIN-D6I7BGDFHRA>
2022-05-02 17:03:23 +0200

    Добавьте файлы проекта.

---
+++ ClassWork03.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    double Farangeitiki = 1.8000;
+
+
+    double celsius;
+    std::cout << "Celsius = ";
+    std::cin >> celsius;
+
+
+    double farangate;
+    std::cout << "Farangates = ";
+    std::cin >> farangate;
+
+
+    double AnswerCelsiusToFarangate = celsius * Farangeitiki;
+    double AnswerFarangateToCelsius = farangate / Farangeitiki;
+
+
+    std::cout << "Celsius to farangate = " << AnswerCelsiusToFarangate << "\n";
+    std::cout << "Farangate to celsius = " << AnswerFarangateToCelsius << "\n";
+}
